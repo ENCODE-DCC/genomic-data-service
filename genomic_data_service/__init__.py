fb906c6df335719a7d5c3a09b472e2197a635beb
Pedro Assis <pedroh@stanford.edu>
2020-10-21 13:51:33 -1000

    setting default configs

--- genomic_data_service/__init__.py
+++ genomic_data_service/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from elasticsearch import Elasticsearch
+from os import environ
 
 
 app = Flask(__name__)
-app.config.from_envvar('GENOMIC_DATA_SERVICE_SETTINGS')
+
+if 'GENOMIC_DATA_SERVICE_SETTINGS' in environ:
+    app.config.from_envvar('GENOMIC_DATA_SERVICE_SETTINGS')
+else:
+    print('[CONFIG] Defaulting to development config')
+    app.config.from_pyfile('../config/development.cfg')
+
 
 es = Elasticsearch(port=app.config['ES_PORT'], hosts=app.config['ES_HOSTS'])
 
