eca3e95201894b297de9e7a36f89e2611fc4ed69
Pedro Assis <pedroh@stanford.edu>
2021-01-25 10:18:20 -1000

    many-to-many relationships fixed

--- genomic_data_service/models.py
+++ genomic_data_service/models.py
@@ -26,6 +26,11 @@ class Expression(db.Model):
         backref=db.backref('expressions', lazy=True))
 
 
+file_tags = db.Table('file_tags',
+    db.Column('file_id', db.String(), db.ForeignKey('files.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True))
+
+
 class File(db.Model):
     __tablename__ = 'files'
 
@@ -40,7 +45,13 @@ class File(db.Model):
     study = db.relationship('Study',
         backref=db.backref('files', lazy=True))
 
-    tags = db.relationship('Tag', backref='file')
+    tags = db.relationship('Tag', secondary=file_tags, lazy='subquery',
+        backref=db.backref('files', lazy=True))
+
+
+project_tags = db.Table('project_tags',
+    db.Column('project_id', db.String(), db.ForeignKey('projects.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True))
 
 
 class Project(db.Model):
@@ -51,7 +62,13 @@ class Project(db.Model):
     version = db.Column(db.String(), nullable=False)
     name = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=True)
-    tags = db.relationship('Tag', backref='project')
+    tags = db.relationship('Tag', secondary=project_tags, lazy='subquery',
+        backref=db.backref('projects', lazy=True))
+
+
+study_tags = db.Table('study_tags',
+    db.Column('study_id', db.String(), db.ForeignKey('studies.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True))
 
 
 class Study(db.Model):
@@ -63,7 +80,8 @@ class Study(db.Model):
     name = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=True)
     parent_project_id = db.Column(db.String(), nullable=False)
-    tags = db.relationship('StudyTags', backref='study')
+    tags = db.relationship('Tag', secondary=study_tags, lazy='subquery',
+        backref=db.backref('studies', lazy=True))
 
 
 class Tag(db.Model):
@@ -72,30 +90,4 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
-
-class StudyTags(db.Model):
-    __tablename__ = 'study_tags'
-
-    study_id = db.Column(db.String(), db.ForeignKey('studies.id'),
-                         primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'),
-                       primary_key=True)
-    
-
-class ProjectTags(db.Model):
-    __tablename__ = 'project_tags'
-
-    project_id = db.Column(db.String(), db.ForeignKey('projects.id'),
-        primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'),
-        primary_key=True)
-
-
-class FileTags(db.Model):
-    __tablename__ = 'file_tags'
-
-    file_id = db.Column(db.String(), db.ForeignKey('files.id'),
-        primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'),
-        primary_key=True)
     
