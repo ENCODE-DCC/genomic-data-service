ecf79300d91b167575f3c76573599974fdefe4cf
Pedro Assis <pedroh@stanford.edu>
2021-02-08 13:29:32 -1000

    adding data ingestion script

--- genomic_data_service/models.py
+++ genomic_data_service/models.py
@@ -1,62 +1,6 @@
 from genomic_data_service import db
-
-class Feature(db.Model):
-    __tablename__ = 'features'
-
-    gene_id = db.Column(db.String(), primary_key=True)
-    transcript_id = db.Column(db.String(), primary_key=True)
-
-    gene_name = db.Column(db.String(), nullable=True)
-    transcript_name = db.Column(db.String(), nullable=True)
-
-
-class Expression(db.Model):
-    __tablename__ = 'expressions'
-
-    id = db.Column(db.String(), primary_key=True)
-    transcript_id = db.Column(db.String())
-    dataset_accession = db.Column(db.String())
-    file_id = db.Column(db.String(), db.ForeignKey('files.id'))
-
-    tpm = db.Column(db.String())
-    fpkm = db.Column(db.String())
-    assembly = db.Column(db.String())
-    assembly_version = db.Column(db.String())
-
-    FILTERS = []
-    TSV_HEADERS = ['featureID', 'tpm']
-
-    # tsv column name => object key
-    TSV_MAP = {
-        'featureID': 'transcript_id',
-        'tpm': 'tpm'
-    }
-    
-    file = db.relationship('File',
-        backref=db.backref('expressions', lazy=True))
-
-    def to_tsv_row(self):
-        return [self.transcript_id, self.tpm]
-    
-
-class File(db.Model):
-    __tablename__ = 'files'
-
-    DEFAULT_UNITS = 'TPM'
-    DEFAULT_FORMAT = 'tsv'
-    ACCEPTED_FORMATS = ['tsv']
-
-    id = db.Column(db.String(), primary_key=True)
-
-    file_type = db.Column(db.String(), nullable=False)
-    url = db.Column(db.String(), nullable=False)
-    version = db.Column(db.String(), nullable=False)
-    md5 = db.Column(db.String(), nullable=False)
-
-    study_id = db.Column(db.String(), db.ForeignKey('studies.id'),
-                         nullable=False)
-    study = db.relationship('Study',
-        backref=db.backref('files', lazy=True))
+import csv
+import datetime
 
 
 class Project(db.Model):
@@ -69,7 +13,7 @@ class Project(db.Model):
     description = db.Column(db.String(), nullable=True)
 
     FILTERS = []
-    
+
     def to_dict(self):
         return {
             'version': self.version,
@@ -95,7 +39,7 @@ class Study(db.Model):
         backref=db.backref('studies', lazy=True))
 
     FILTERS = []
-    
+
     def to_dict(self):
         return {
             'version': self.version,
@@ -105,3 +49,139 @@ class Study(db.Model):
             'name': self.name,
             'genome': self.genome
         }
+
+
+class Feature(db.Model):
+    __tablename__ = 'features'
+
+    gene_id = db.Column(db.String(), primary_key=True)
+    transcript_id = db.Column(db.String(), primary_key=True)
+
+    gene_name = db.Column(db.String(), nullable=True)
+    transcript_name = db.Column(db.String(), nullable=True)
+
+
+class Expression(db.Model):
+    __tablename__ = 'expressions'
+
+    id = db.Column(db.String(), primary_key=True)
+    feature_id = db.Column(db.String(), nullable=False)
+    feature_type = db.Column(db.String(), nullable=False)
+    dataset_accession = db.Column(db.String())
+    file_id = db.Column(db.String(), db.ForeignKey('files.id'))
+
+    tpm = db.Column(db.String())
+    fpkm = db.Column(db.String())
+
+    FILTERS = []
+    TSV_HEADERS = ['featureID', 'tpm']
+
+    # tsv column name => object key
+    TSV_MAP = {
+        'featureID': 'feature_id',
+        'tpm': 'tpm'
+    }
+    
+    file = db.relationship('File',
+        backref=db.backref('expressions', lazy=True))
+
+    def to_tsv_row(self):
+        return [self.transcript_id, self.tpm]
+    
+
+class File(db.Model):
+    __tablename__ = 'files'
+
+    DEFAULT_UNITS = 'TPM'
+    DEFAULT_FORMAT = 'tsv'
+    ACCEPTED_FORMATS = ['tsv']
+
+    id = db.Column(db.String(), primary_key=True)
+
+    file_type = db.Column(db.String(), nullable=False)
+    url = db.Column(db.String(), nullable=False)
+    version = db.Column(db.String(), nullable=False)
+    md5 = db.Column(db.String(), nullable=False)
+    assay = db.Column(db.String(), nullable=False)
+    assembly = db.Column(db.String(), nullable=False)
+    assembly_version = db.Column(db.String(), nullable=False)
+
+    file_indexed_at = db.Column(db.String())
+
+    study_id = db.Column(db.String(), db.ForeignKey('studies.id'),
+                         nullable=False)
+    study = db.relationship('Study',
+        backref=db.backref('files', lazy=True))
+
+
+    def fetch_and_parse(self):
+        file = requests.get(self.url)
+
+        if file.status_code != 200:
+            print("Failed to download file " + self.url)
+            return None
+
+        if self.file_type == 'tsv':
+            expression_data = file.content.decode("utf-8").splitlines()
+            return(csv.reader(expression_data , delimiter="\t", quotechar='"'))
+
+        return None
+
+
+    def import_expressions(self):
+        if file_indexed_at:
+            return
+
+        parsed_file = self.fetch_and_parse()
+
+        if not parsed_file:
+            return
+
+        header = next(parsed_file)
+
+        fields = {
+            'gene_id': header.index('gene_id'),
+            'transcript_ids': header.index('transcript_id(s)'),
+            'tpm': header.index('TPM'),
+            'fpkm': header.index('FPKM')
+        }
+
+        expressions = []
+        features = []
+
+        for row in parsed_file:
+            gene_id = row[fields['gene_id']]
+            transcript_ids = row[fields['transcript_ids']].split(',')
+
+            expressions.append(
+                Expression(
+                    id=uuid.uuid4(),
+                    feature_id=gene_id,
+                    feature_type='gene_id',
+                    dataset_accession=self.study_id,
+                    tpm=row[fields['tpm']],
+                    fpkm=row[fields['fpkm']],
+                    file_id=self.id,
+                    assembly=self.assembly,
+                    assembly_version=self.assembly_version
+                )
+            )
+
+            for transcript_id in transcript_ids:
+                features.append(
+                    Feature(
+                        gene_id=gene_id,
+                        transcript_id=transcript_id
+                    )
+                )
+
+        for expression in expressions:
+            db.session.add(expression)
+
+        for feature in features:
+            db.session.add(feature)
+
+        self.file_indexed_at = str(datetime.datetime.now())
+        db.session.add(self)
+
+        db.session.commit()
---
+++ genomic_data_service/rna_seq_ingestion.py
@@ -0,0 +1,156 @@
+from genomic_data_service import db
+from genomic_data_service.models import Feature, Expression, File, Project, Study
+import requests
+import csv
+import uuid
+import pickle
+
+import os.path
+from os import path
+
+from celery import Celery
+
+
+celery_app = Celery('rna_seq_ingestion', broker='redis://localhost')
+
+
+def extract_project(metadata):
+    award = metadata['award']
+
+    id = award['uuid']
+
+    exists = Project.query.filter(Project.id == id).first()
+    if exists:
+        return exists
+
+    return Project(
+        id=id,
+        version=award['schema_version'],
+        name=award['project'],
+        description=award.get('description')
+    )
+
+
+def extract_study(metadata, parent_project_id):    
+    experiment = requests.get("https://www.encodeproject.org" + metadata['dataset'] + "?format=json")
+
+    if experiment.status_code != 200:
+        print("Failed to acquire metadata for " + metadata['dataset'])
+        return None
+
+    if 'genome_annotation' not in metadata:
+        print("Genome annotation not found for " + metadata['dataset'] + ". Skipping...")
+        return None
+
+    experiment = experiment.json()
+    genome = metadata['assembly'] + ' ' + metadata.get('genome_annotation', '')
+
+    exists = Study.query.filter(Study.id == experiment['accession']).first()
+    if exists:
+        return exists
+
+    return Study(
+        id=experiment['accession'],
+        name=experiment.get('description', 'Study ' + experiment['accession']),
+        version=experiment['date_released'],
+        description=experiment.get('description'),
+        parent_project_id=parent_project_id,
+        genome=genome
+    )
+
+
+def extract_file(metadata, study_id):
+    if metadata['file_format'] not in File.ACCEPTED_FORMATS:
+        print("Unprocessable file format " + metadata['file_format'] + " for " + metadata['href'])
+        return None
+
+    exists = File.query.filter(File.id == metadata['accession']).first()
+    if exists:
+        return exists
+
+    return File(
+        id=metadata['accession'],
+        file_type=metadata['file_type'],
+        url="https://encodeproject.org"+metadata['href'],
+        version=metadata['date_created'],
+        study_id=study_id,
+        md5=metadata['md5sum'],
+        assay=metadata['assay_term_name'],
+        assembly=metadata['assembly'],
+        assembly_version=metadata['genome_annotation']
+    )
+
+
+def fetch_reject_list():
+    print("Fetching rejection list...")
+
+    reject_list_url = "https://www.encodeproject.org/search/?type=File&status=released&output_type=gene+quantifications&assembly=mm10&assay_term_name!=single-cell+RNA+sequencing+assay&limit=all&format=json"
+
+    reject_response = requests.get(reject_list_url).json()
+
+    reject_accessions = set([])
+    for item in reject_response['@graph']:
+        reject_accessions.add(item['accession'])
+
+    return reject_accessions
+        
+
+def get_all_accessions():
+    pickle_filename = 'rna_seq_accessions.pickle'
+
+    if path.exists(pickle_filename):
+        return pickle.load(open(pickle_filename, 'rb'))
+
+    reject_list = fetch_reject_list()
+
+    print("Fetching files list...")
+    gene_quantifications_list = "https://www.encodeproject.org/search/?type=File&status=released&output_type=gene+quantifications&assembly=GRCh38&limit=all&format=json"
+
+    files_metadata = requests.get(gene_quantifications_list).json()
+
+    accessions_to_process = set([])
+    for entry in files_metadata['@graph']:
+        if entry['accession'] not in reject_list:
+            accessions_to_process.add(entry['accession'])
+
+    with open(pickle_filename, 'wb') as output:
+        pickle.dump(accessions_to_process, output)
+
+    return accessions_to_process
+
+
+@celery_app.task(bind=True, name='genomic_data_service.rna_seq_ingestion.process_accession', autoretry_for=(Exception,), retry_kwargs={'max_retries': 3})
+def process_accession(self, accession):
+    print("Processing " + accession + " ...")
+    metadata = requests.get("https://www.encodeproject.org/files/" + accession + "?format=json")
+
+    if metadata.status_code != 200:
+        print("Failed to acquire metadata for " + accession)
+        return
+
+    metadata = metadata.json()
+
+    project = extract_project(metadata)
+    study = extract_study(metadata, project.id)
+
+    file = None
+    if study:
+        file = extract_file(metadata, study.id)
+
+    for entry in [project, study, file]:
+        if entry:
+            db.session.add(entry)
+
+    db.session.commit()
+
+
+def index_all_accessions():
+    accessions = sorted(list(get_all_accessions()))
+
+    for accession in accessions:
+        process_accession.delay(accession)
+
+
+if __name__ == "__main__":
+    index_all_accessions()
+
