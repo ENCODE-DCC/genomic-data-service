264a137fe9640f2b4971be4e9d62a642f2cc9818
Keenan Graham <keenangraham@stanford.edu>
2021-06-30 19:01:24 -0700

    Add load genes and datasets and tests

--- genomic_data_service/rnaseq/remote/portal.py
+++ genomic_data_service/rnaseq/remote/portal.py
@@ -1,5 +1,18 @@
 import requests
 
+from genomic_data_service.rnaseq.domain.constants import GENES
+from genomic_data_service.rnaseq.domain.constants import DATASETS
+from genomic_data_service.rnaseq.domain.gene import get_genes_by_ensembl_id
+from genomic_data_service.rnaseq.domain.gene import Gene
+
+from genomic_data_service.rnaseq.remote.constants import AT_GRAPH
+from genomic_data_service.rnaseq.remote.constants import BASE_URL
+from genomic_data_service.rnaseq.remote.constants import FILE_PARAMS
+from genomic_data_service.rnaseq.remote.constants import DATASET_PARAMS
+from genomic_data_service.rnaseq.remote.constants import GENE_PARAMS
+from genomic_data_service.rnaseq.remote.constants import SEARCH_PATH
+
+
 
 def get_json(url):
     response = requests.get(url)
@@ -8,8 +21,39 @@ def get_json(url):
 
 class Portal:
 
+    BASE_URL = BASE_URL
+
     def __init__(self):
-        pass
+        self.repositories = {}
+
+    def _get_gene_url(self):
+        return self.BASE_URL + SEARCH_PATH + GENE_PARAMS
+
+    def _load_genes(self):
+        url = self._get_gene_url()
+        genes = (
+            Gene(props)
+            for props in get_json(url)[AT_GRAPH]
+        )
+        self.repositories[GENES] = get_genes_by_ensembl_id(
+            genes
+        )
+
+    def _get_dataset_url(self):
+        return self.BASE_URL + SEARCH_PATH + DATASET_PARAMS
+
+    def _load_datasets(self):
+        url = self._get_dataset_url()
+        self.repositories[DATASETS] = {
+            dataset['@id']: dataset
+            for dataset in get_json(url)[AT_GRAPH]
+        }
+
+    def _get_file_url(self):
+        return self.BASE_URL + SEARCH_PATH + FILE_PARAMS
 
     def get_rna_seq_files(self):
-        pass
+        self._load_genes()
+        self._load_datasets()
+        url = self._get_file_url()
+        return get_json(url)[AT_GRAPH]
