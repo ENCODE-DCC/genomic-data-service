c0f5f7cdd46b4df206959de9591dc75596b73803
Keenan Graham <keenangraham@stanford.edu>
2021-08-13 18:37:17 -0700

    Run unit tests from param parser with dummy request from Flask

--- genomic_data_service/searches/adapters/requests.py
+++ genomic_data_service/searches/adapters/requests.py
@@ -1,19 +1,55 @@
+from flask import Request
+from werkzeug.datastructures import ImmutableOrderedMultiDict
+
+
 class ArgsAdapter:
 
     def __init__(self, args):
         self.args = args
 
+    def __iter__(self):
+        return self.args.__iter__()
+
     def items(self):
         return self.args.items(multi=True)
+
+    def getall(self, key):
+        return self.args.getlist(key)
 
 
 class RequestAdapter:
 
     def __init__(self, request):
         self._request = request
-        self._params = ArgsAdapter(request.args)
 
     @property
     def params(self):
-        return self._params
-        
+        return ArgsAdapter(
+            self._request.args
+        )
+
+    def __setitem__(self, key, value):
+        Request.parameter_storage_class = ImmutableOrderedMultiDict
+        self._request = Request({key: value})
+
+    def __getitem__(self, key):
+        return self._request.environ[key]
+
+    @property
+    def environ(self):
+        return self
+
+    @property
+    def query_string(self):
+        return self._request.query_string.decode('utf-8')
+
+    @query_string.setter
+    def query_string(self, value):
+        self.__setitem__('QUERY_STRING', value)
+
+    def copy(self):
+        return RequestAdapter(
+            Request(
+                self._request.environ
+            )
+        )
--- genomic_data_service/searches/tests/test_searches_adapters_requests.py
+++ genomic_data_service/searches/tests/test_searches_adapters_requests.py
@@ -42,3 +42,70 @@ def test_searches_adapters_requests_request_adapter_params():
         ('files.file_type', 'bed bed3+'),
         ('accession!', 'ABC')
     ]
+
+
+def test_searches_adapters_request_works_with_params_parsers():
+    from genomic_data_service.searches.adapters.requests import RequestAdapter
+    from flask import Request
+    from snosearch.parsers import ParamsParser
+    request = Request(
+        {
+            'QUERY_STRING': 'type!=Experiment'
+        }
+    )
+    pp = ParamsParser(RequestAdapter(request))
+    assert 'type!' in pp._request.params
+    assert pp.is_param('type!', 'Experiment')
+    assert not pp.is_param('type', 'RNAExpression')
+    assert pp.get_filters_by_condition() == [
+        ('type!', 'Experiment')
+    ]
+
+
+def test_searches_adapters_request_works_with_params_parser_get_filters_by_condition():
+    from genomic_data_service.searches.adapters.requests import RequestAdapter
+    from flask import Request
+    from snosearch.parsers import ParamsParser
+    request = Request(
+        {
+            'QUERY_STRING': 'type=Experiment&type=File&field=status'
+        }
+    )
+    pp = ParamsParser(RequestAdapter(request))
+    assert pp.get_filters_by_condition(
+        key_and_value_condition=lambda k, _: k == 'field'
+    ) == [
+        ('field', 'status')
+    ]
+    assert pp.get_filters_by_condition(
+        key_and_value_condition=lambda k, _: k == 'type'
+    ) == [
+        ('type', 'Experiment'),
+        ('type', 'File')
+    ]
+    assert pp.get_search_term_filters() == []
+    assert pp.get_must_match_filters() == [
+        ('type', 'Experiment'),
+        ('type', 'File'),
+        ('field', 'status')
+    ]
+
+
+def test_searches_adapters_request_adapter_passes_param_parser_unit_tests():
+    from genomic_data_service.searches.adapters.requests import RequestAdapter
+    from flask import Request
+    from snosearch.parsers import ParamsParser
+    from snosearch.tests import test_searches_parsers
+    tests = {
+        k: v
+        for k, v in vars(test_searches_parsers).items()
+        if k.startswith('test')
+    }
+    dummy_request = RequestAdapter(Request({}))
+    for test_name, test in tests.items():
+        dummy_request['QUERY_STRING'] = ''
+        print(test_name)
+        try:
+            test(dummy_request)
+        except (ModuleNotFoundError):
+            print('skipping')
