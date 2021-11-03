7ec66778b1470ed68eb95e855cbf52e332fb8d50
Keenan Graham <keenangraham@stanford.edu>
2021-11-03 14:43:36 -0700

    Add way to stream ndjson

---
+++ genomic_data_service/reports/ndjson.py
@@ -0,0 +1,27 @@
+import json
+
+from flask import Response
+
+
+class NDJSONGenerator:
+    '''
+    Streams list[dicts] as newline-delimited JSON.
+    '''
+
+    NEWLINE = '\n'
+    MIMETYPE = 'application/x-ndjson'
+
+    def __init__(self, values):
+        self.values = values
+
+
+    def stream(self):
+        for value in self.values:
+            yield json.dumps(value) + '\n'
+        
+
+    def as_response(self):
+        return Response(
+            self.stream(),
+            mimetype=self.MIMETYPE,
+        )
