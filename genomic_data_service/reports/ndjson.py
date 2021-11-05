0574be5be78d976f111d7bb53a9509823e2cd8e6
Keenan Graham <keenangraham@stanford.edu>
2021-11-05 11:38:07 -0700

    Single line

--- genomic_data_service/reports/ndjson.py
+++ genomic_data_service/reports/ndjson.py
@@ -14,11 +14,9 @@ class NDJSONGenerator:
     def __init__(self, values):
         self.values = values
 
-
     def stream(self):
         for value in self.values:
             yield json.dumps(value) + '\n'
-        
 
     def as_response(self):
         return Response(
