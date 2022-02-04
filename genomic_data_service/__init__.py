7d3bcbc47f8a6334edc774de2400d94c5e1e17af
Mingjie Li <mingjiecn@gmail.com>
2022-02-04 14:37:00 -0600

    fix format

--- genomic_data_service/__init__.py
+++ genomic_data_service/__init__.py
@@ -7,17 +7,18 @@ from genomic_data_service.rnaseq.rnaget.api import rnaget_api
 
 
 def is_web_app():
-    return ('FLASK_APP' in environ)
+    return "FLASK_APP" in environ
+
 
 app = Flask(__name__)
 app.register_blueprint(rnaget_api)
 
 
-if 'GENOMIC_DATA_SERVICE_SETTINGS' in environ:
-    app.config.from_envvar('GENOMIC_DATA_SERVICE_SETTINGS')
+if "GENOMIC_DATA_SERVICE_SETTINGS" in environ:
+    app.config.from_envvar("GENOMIC_DATA_SERVICE_SETTINGS")
 else:
-    print('[CONFIG] Defaulting to development config')
-    app.config.from_pyfile('../config/development.cfg')
+    print("[CONFIG] Defaulting to development config")
+    app.config.from_pyfile("../config/development.cfg")
 
 add_registry(app)
 add_rna_client(app)
@@ -25,32 +26,36 @@ add_rna_client(app)
 
 if is_web_app():
     from elasticsearch import Elasticsearch
-    regulome_es = Elasticsearch(port=app.config['REGULOME_ES_PORT'], hosts=app.config['REGULOME_ES_HOSTS'])
-    region_search_es = Elasticsearch(port=app.config['REGION_SEARCH_ES_PORT'], hosts=app.config['REGION_SEARCH_ES_HOSTS'])
+
+    regulome_es = Elasticsearch(
+        port=app.config["REGULOME_ES_PORT"], hosts=app.config["REGULOME_ES_HOSTS"]
+    )
+    region_search_es = Elasticsearch(
+        port=app.config["REGION_SEARCH_ES_PORT"],
+        hosts=app.config["REGION_SEARCH_ES_HOSTS"],
+    )
     app.url_map.strict_slashes = False
 
     # Enabled endpoints:
     import genomic_data_service.search
     import genomic_data_service.summary
-    
-    @app.route('/healthcheck/', methods=['GET'])
+
+    @app.route("/healthcheck/", methods=["GET"])
     def healthcheck():
         status = {}
         try:
-            status['regulome_es'] = regulome_es.cluster.health()
-            status['region_search_es'] = region_search_es.cluster.health()
+            status["regulome_es"] = regulome_es.cluster.health()
+            status["region_search_es"] = region_search_es.cluster.health()
         except Exception as e:
-            status['exception'] = str(e)
-
+            status["exception"] = str(e)
 
         response_out = make_response(
             jsonify(status),
-            200,                
+            200,
         )
-        response_out.headers = {
-            "Content-Type": "application/json"
-        }
+        response_out.headers = {"Content-Type": "application/json"}
         return response_out
+
 
 import genomic_data_service.errors
 import genomic_data_service.rnaseq.views
