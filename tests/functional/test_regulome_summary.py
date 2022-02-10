9c5181dbb054c4806bc48217315170d1f89642a2
Mingjie Li <mingjiecn@gmail.com>
2022-02-10 14:39:11 -0600

    add unit test to test summary limit

--- tests/functional/test_regulome_summary.py
+++ tests/functional/test_regulome_summary.py
@@ -19,3 +19,19 @@ def test_valid_params_limit_1(test_client):
         "summary/?regions=rs75982468%0D%0Ars7745856&genome=GRCh37&maf=0.01&limit=1"
     )
     assert response.status_code == 200
+
+
+def test_valid_params_limit_all(test_client):
+
+    response = test_client.get(
+        "summary/?regions=rs75982468%0D%0Ars7745856&genome=GRCh37&maf=0.01&limit=all"
+    )
+    assert response.status_code == 200
+
+
+def test_valid_params_limit_exception(test_client):
+
+    response = test_client.get(
+        "summary/?regions=rs75982468%0D%0Ars7745856&genome=GRCh37&maf=0.01&limit=no"
+    )
+    assert response.status_code == 200
