b351f460157884b5991db966a49bf9979db81caf
Mingjie Li <44071821+mingjiecn@users.noreply.github.com>
2022-02-17 15:10:04 -0600

    DSERV-3_rsid_coordinates_resolver_unit_test (#29)

--- tests/functional/test_elasticsearch.py
+++ tests/functional/test_elasticsearch.py
@@ -663,3 +663,21 @@ def test_regulome_summary_tsv(test_client):
     )
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "text/tsv"
+
+
+def test_regulome_search_total_1(test_client):
+    response = test_client.get("search/?regions=rs10823321&genome=GRCh37")
+    assert response.status_code == 200
+    res = response.json
+    assert res["query_coordinates"] == ["chr10:70989269-70989270"]
+    assert res["assembly"] == "GRCh37"
+    assert res["total"] == 1
+
+
+def test_regulome_search_total_0(test_client):
+    response = test_client.get("search/?regions=rs56116432&genome=GRCh37")
+    assert response.status_code == 200
+    res = response.json
+    assert res["query_coordinates"] == []
+    assert res["assembly"] == "GRCh37"
+    assert res["total"] == 0
