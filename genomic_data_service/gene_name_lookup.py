9d605de93904b5705dcd7e43af64e597b05c270a
Mingjie Li <mingjiecn@gmail.com>
2022-03-24 18:28:08 -0500

    increase the coverage

--- genomic_data_service/gene_name_lookup.py
+++ genomic_data_service/gene_name_lookup.py
@@ -1,15 +1,12 @@
 import requests
+
+
 def gene_name_lookup(ensemblId):
     endpoint = "https://www.encodeproject.org/search/?type=Gene&organism.scientific_name=Homo+sapiens&format=json"
     dbxrefs = "&dbxrefs=ENSEMBL:" + ensemblId
     endpoint = endpoint + dbxrefs
-    try:
-        gene = requests.get(endpoint).json()["@graph"]
-    except Exception:
-        return None
+    gene = requests.get(endpoint).json()["@graph"]
     if gene:
         return gene[0]["symbol"]
     else:
         return None
-
-
