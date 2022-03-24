49d697125d7396b74cdde3174547bcc7331ef045
Mingjie Li <mingjiecn@gmail.com>
2022-03-24 17:51:33 -0500

    add tests/functional/test_gene_name_lookup.py

---
+++ tests/functional/test_gene_name_lookup.py
@@ -0,0 +1,11 @@
+import pytest
+from genomic_data_service.gene_name_lookup import gene_name_lookup
+
+def test_gene_name_lookup():
+    gene_name = gene_name_lookup("ENSG00000162367")
+    assert gene_name == "TAL1"
+
+def test_gene_name_lookup_none():
+    gene_name = gene_name_lookup("ENSGI00000162367")
+    assert gene_name == None
+    
